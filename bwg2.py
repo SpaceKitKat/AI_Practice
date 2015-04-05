37f3a66899446c925139a45c7996acf1db151cd5
root <root@KITT.(none)>
2015-04-04 17:32:25 -0700

    progress on partB. //TODO: add dating advice and jokes

--- bwg2.py
+++ bwg2.py
@@ -2,24 +2,27 @@
 #author: Bilkit Githinji
 
 from re import *   # Loads the regular expression module.
+# keep track of conversation info: name, gender, topic, etc.
+MEMORY = {}
+swear_count = 0
 
-MEMORY = {} # keep track of conversation info
 def Grandpa():
   'Grandpa is the top-level function, containing the main loop.'
   print(intro())
   while True:
     the_input = input('TYPE HERE:>> ')
     if match('bye',the_input):
-        print('Eh, the hell with ya!')
-        return
-
-    print(respond(the_input))
+      print('Goodbye!')
+      return
+    if swear_count > 5:
+      print('Eh, the hell with ya!')
+      return
+    print("\n"+respond(the_input)+"\n")
 
 def intro():
   MEMORY['topic'] = 'intro'
-  return "Hey there! They call me "+agentName()+""". I'm one of Bilkit Githinji's
-oldest agents. I know a lot about life and love.
-What\'s yer name kiddo?"""
+  return "Hey there! They call me "+agentName()+""". I'm one of Bilkit Githinji's oldest agents.\
+  I know a lot about love and I have a great sense of humor! What\'s yer name kiddo?"""
 
 def agentName():
   return "Ol\' Gramps"
@@ -36,54 +39,74 @@ def respond(the_input):
   mapped_wordlist[0]=mapped_wordlist[0].capitalize()
   print("memory: "+str(MEMORY)) ## INFO ##
   # process word list and compile response
-  if wordlist[0]=='':
-    # tell a joke
-    return tell_joke()
+  if containsSwears(wordlist):
+    return condemn_swearing()
+  # start off with introduction: learn about gender
   if MEMORY['topic'] == 'intro':
     if 'boy' in wordlist: # switch topic to relationship
       MEMORY['gender'] = 'male'
       MEMORY['topic']  = 'relationship_intro'
-      return "Do you have a girlfriend? What's her name?"
+      return "Aww, I bet you are handsome. Do you have a girlfriend? What's her name?"
     if 'girl' in wordlist: # switch topic to relationship
       MEMORY['gender'] = 'female'
       MEMORY['topic']  = 'relationship_intro'
-      return "Do you have a boyfriend? What's his name?"
+      return "Aww, I bet you are pretty. Do you have a boyfriend? What's his name?"
     if len(wordlist) == 1:
-      MEMORY['name'] = wordlist
-      return wordlist.capitalize()+"? What kinda names are they giving you kids nowadays." \
-                                         "So"+wordlist.capitalize()+", are you a boy or girl?"
+      MEMORY['name'] = wordlist[0]
+      return MEMORY['name'].capitalize()+"? What kinda names are they giving you kids nowadays?!"\
+          " So "+MEMORY['name'].capitalize()+", are you a boy or girl?"
     if ['my','name'] == wordlist[0:2]:
       MEMORY['name'] = wordlist[3]
-      return "Nice to meet ya, "+ wordlist[3].capitalize()+"! So"+wordlist[3].capitalize()+\
+      return "Nice to meet ya, "+ MEMORY['name'].capitalize()+"! So "+MEMORY['name'].capitalize()+\
              ", are you a boy or girl?"
-    return punt() # intro default
-  # yes and topic is relation ask about name
-  # save name of partner and offer relationship advice
+    return "Come again?" # intro default
+  # decide what advice to offer; relationship or dating
   if MEMORY['topic'] == 'relationship_intro':
     if 'no' in wordlist or 'don\'t' in wordlist or 'do not' in wordlist: # switch topic to dating advice
+      MEMORY['topic'] = 'dating_advice'
       return #dating advice
     if wordlist[0] == 'yes' or 'name' in wordlist: # save partner's name
       MEMORY['topic'] = 'relationship_advice'
-      MEMORY['partner'] = wordlist[wordlist.index('name')+2]
+      if 'name' in wordlist:                       # capitalize their partner's name
+        MEMORY['partner'] = wordlist[wordlist.index('name')+2]; MEMORY['partner'].capitalize()
+      else:
+        MEMORY['partner'] = 'your honey bunny'     # if no name, call assume a silly one
       # initialize advice including partner's name
-      wisdom = ['You should say \"I love you\" to '+MEMORY['partner'].capitalize()+' every day.',
-                'Hey, take it from me. Don\'t rush into having kids with '+MEMORY['partner'].capitalize()+'.']
+      wisdom = ['You should say \"I love you\" to '+MEMORY['partner']+' every day.',
+                'Hey, take it from me. Don\'t rush into having kids with '+MEMORY['partner']+'.']
       return relationship_advice(wisdom)
+  # give relationship advice cyclically. Ol' Gramps wants the other agent to get his point!
   if MEMORY['topic'] == 'relationship_advice':
     # handle questions
     if 'i' in wordlist[0]:
       return "I hope you really "+stringify(mapped_wordlist[1:])
-    if 'ok' in wordlist[0] or 'okay' in wordlist[0]:
-      return # some change in topic
+    # change topics
+    if 'thanks' in wordlist or 'thank you' in wordlist:
+
+      return "Sure thing! What else "
+    if 'ok' == wordlist[0] or 'okay' == wordlist[0] or 'yeah' == wordlist[0] or 'sure' == wordlist[0]:
+      moreAdvice = relationship_advice(wisdom);
+      return 'And another thing, '+moreAdvice.lower()
+
+
     return relationship_advice(wisdom)
+  # give dating advice randomly; it's been a while since Ol' Gramps had a date
   if MEMORY['topic'] == 'dating_advice':
+
+
     return # dating advice default
+  # otherwise tell jokes to cheer up the agents and Ol' Gramps himself
   if 'sorry' in wordlist:
     return "Don't be sorry! Let's have a laugh. "+tell_joke()
   if 'health' in wordlist or 'healthy' in wordlist:
     return "Well, my friends are all dead :(. Unlike them, I still feel alive! Yipee!"
+  #
+  if verbp(wordlist[0]):
+      print("I'm afraid I'm too old to " +\
+            stringify(mapped_wordlist) + '?')
+      return
   # default: punt phrase
-  return punt()
+  return "Time for a laugh. "+tell_joke()
 
 def stringify(wordlist):
   'Create a string from wordlist, but with spaces between words.'
@@ -102,35 +125,35 @@ def wpred(w):
 def dpred(w):
   'Returns True if w is an auxiliary verb.'
   return (w in ['do','can','should','would'])
-# cyclical responses
 
-PUNTS           = ['Come again?\n']
-WISDOM_DATING   = []
-punt_count = 0
+WISDOM_DATING   = ['']
+CONDEMNATIONS   = ['Do you kiss your mother with that mouth?!',
+                   'Your mother ought to wash your mouth out with soap!',
+                   'Why am I alive only to listen to this foul speech?!,']
+rel_count = 0
+# cyclic responses
 def relationship_advice(wisdom):
   'Returns one from a list of wise responses. Uses cyclic selection.'
-  global punt_count
-  punt_count += 1
-  return wisdom[punt_count % len(wisdom)]
+  global rel_count
+  rel_count += 1
+  return wisdom[rel_count % len(wisdom)]
+def condemn_swearing():
+  global swear_count
+  swear_count += 1
+  return CONDEMNATIONS[(swear_count-1)%len(CONDEMNATIONS)]
+# random responses
 def dating_advice():
   'Returns one from a list of wise responses. Uses cyclic selection.'
-  global punt_count
-  punt_count += 1
-  return WISDOM_DATING[punt_count % len(WISDOM_DATING)]
-def punt():
-  'Returns one from a list of wise responses. Uses cyclic selection.'
-  global punt_count
-  punt_count += 1
-  return PUNTS[punt_count % len(PUNTS)]
-
-# random responses
+  global dat_count
+  dat_count += 1
+  return WISDOM_DATING[dat_count % len(WISDOM_DATING)]
 DADJOKES = ['What do you call a fish with no eyes? A fshhhh.'
             'How long does it take a pirate to say the alphabet? Forever, because they spend years at \'C\'']
 def tell_joke():
   'Returns one from a list of default responses.'
-  global punt_count
-  punt_count += 1
-  return DADJOKES[punt_count % len(DADJOKES)]
+  global joke_count
+  joke_count += 1
+  return DADJOKES[joke_count % len(DADJOKES)]
 
 
 CASE_MAP = {'i':'you', 'I':'you', 'me':'you','you':'me',
@@ -148,12 +171,19 @@ def you_me(w):
 def you_me_map(wordlist):
   'Applies YOU-ME to a whole sentence or phrase.'
   return [you_me(   w) for w in wordlist]
-
+def containsSwears(wl):
+  'Returns True if wl contains a bad word or phrase.'
+  swear = False
+  for w in wl:
+    if w in ['shit', 'fuck', 'damn', 'fucker', 'suck','bullshit']: swear = True
+  return swear
+def swears(w):
+  return ()
 def verbp(w):
   'Returns True if w is one of these known verbs.'
-  return (w in ['go', 'have', 'be', 'try', 'eat', 'take', 'help',
+  return (w in ['go', 'have', 'be', 'try', 'play', 'take', 'help',
                 'make', 'get', 'jump', 'write', 'type', 'fill',
-                'put', 'turn', 'compute', 'think', 'drink',
-                'blink', 'crash', 'crunch', 'add'])
+                'put', 'turn', 'compute', 'hike', 'drink',
+                'drive', 'crash', 'crunch', 'add'])
 
 Grandpa() # Launch the program.
