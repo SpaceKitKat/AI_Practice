a2c4f057e1767ef3a7224f1594ac57e733060fc8
Bilkit <bilkit@KITT.(none)>
2015-05-06 12:49:08 -0700

    finished makemove,minimax,staticEval

--- bwg2KInARow.py
+++ bwg2KInARow.py
@@ -2,6 +2,7 @@
 
 from re import *   # Loads the regular expression module.
 from random import randint
+from copy import deepcopy
 # import FiveInARowGameType as game
 import TicTacToeGameType as game
 
@@ -26,7 +27,12 @@ def prepare(init_state,K,what_side,op_name):
   global ZOBRIST_VALS,DIM,K_WIN,SIDE,OP_NAME
   OP_NAME=op_name
   K_WIN=K
+  # 0 --> height, 1--> width
   DIM=(len(init_state[0]),len(init_state[0][0]))
+  #throw exception if k exceeds boarders
+  if DIM[0] < K_WIN or DIM[1] < K_WIN:
+    raise ValueError("K greater than DIM" )
+    return
 
   if what_side.lower()=='x': SIDE=1
 
@@ -41,83 +47,115 @@ def prepare(init_state,K,what_side,op_name):
 
   return True
 
-#<TEST>#
+
 MAX_PLY=4
+NEG_INF=-100000
+POS_INF=100000
 def makeMove(currState, currRemark, timeLim=10000):
   global ZOBRIST_VALS,Z_HASH_TABLE,DIM,K_WIN,SIDE,OP_NAME
 
-  if SIDE==1: mini_max(currState,'x',MAX_PLY)
-  else: mini_max(currState,'o',MAX_PLY)
-  z_succ = [key for key in Z_HASH_TABLE if (Z_HASH_TABLE[z]!=None) and (Z_HASH_TABLE[key][2]==1)]
+  newboard=[]
+
+  bestEval=mini_max(currState[0],currState[1],MAX_PLY)
+  if bestEval == NEG_INF or bestEval == POS_INF:
+    raise ValueError("MAX_PLY greater than empty spaces on board" )
+    return
+  # find single ply state with matching mini_max score
+  for z in Z_HASH_TABLE:
+    if (Z_HASH_TABLE[z][2] == 1) and (Z_HASH_TABLE[z][1] == bestEval):
+      newboard=Z_HASH_TABLE[z][0]
+  if newboard == []:
+    raise ValueError("new board not generated" )
+    return
+
+  print("newboard\n"+display_board(newboard)+"bestVal="+str(bestEval)) #DEBUG#
+
   #convert to state
 
-  return [[move,newState],newRemark]
+  # newstate=[[newboard],get_other(currState[1])]
+  return #[[move,newState],newRemark]
 
-#pass 'max' of x's move
+#pass whoseMove= 'x' or 'o'
 def mini_max(board,whoseMove,plyRemaining):
-  global ZOBRIST_VALS,Z_HASH_TABLE
+  global Z_HASH_TABLE
 
-  who=whoseMove.lower()
-  if plyRemaining == 0: return staticEval(board)
-  if who=='max': minimax_val=-100000
-  else: minimax_val=100000
-  # if plyRemaining>1: successors =  # all
-  # else: successors = get_successors(board,who)
-  for s in get_successors(board,who):
-    newVal=mini_max(s,get_other(who),plyRemaining-1)
+  successors=[]
+  # determine if max or min player
+  if whoseMove.lower()=='x': who='max'
+  else: who='min'
 
-    if(who=='max' and newVal>minimax_val) or (who=='min' and newVal<minimax_val):
-      minimax_val=newVal
+  if plyRemaining == 0: #leaf node in look ahead
+    print("ply: "+str(MAX_PLY-plyRemaining)+"\n"+display_board(board)+"staticEval="+str(staticEval(board))) #DEBUG#
+    return staticEval(board)
+  if who=='max':
+    minimaxVal=-100000
+    successors=get_successors(board,'x')
+  else:
+    minimaxVal=100000
+    successors=get_successors(board,'o')
+  #DEBUG#
+  print("ply: "+str(MAX_PLY-plyRemaining)+"\twhose turn: "+who+"\n"+display_board(board)+"get successors...")
+  for s in successors:
+    newVal=mini_max(s,get_other(whoseMove),plyRemaining-1)
+    # get max or min static eval score based on whose move
+    if(who=='max' and newVal>minimaxVal) or (who=='min' and newVal<minimaxVal):
+      minimaxVal=newVal; print("ply "+str(MAX_PLY-plyRemaining)+"--> "+str(newVal)) #DEBUG#
+    if MAX_PLY-plyRemaining != 0: # store all but root and leaves in hash table
+      Z_HASH_TABLE[z_hash_code(board)] = [board,newVal,MAX_PLY-plyRemaining]
+    # print("***store\n"+display_board(board)+"val="+str(newVal)+"\tply="+str(MAX_PLY-plyRemaining))#DEBUG#
 
-    Z_HASH_TABLE[z_hash_code(board)]=[board,minimax_val,MAX_PLY-plyRemaining]
-  return minimax_val
-#<TEST>#
+  return minimaxVal
 
-def staticEval(state):
+
+def staticEval(board):
   global K_WIN,DIM,SIDE,UD,LR,D2,D2
   'calculates hs for state 1xNXM'
   count=[]; hs=0
   for ii in range(DIM[0]):
     for jj in range(DIM[1]):
-      sq=state[ii][jj]
+      sq=board[ii][jj]
       # print('square ('+str(ii)+','+str(jj)+')='+str(sq)) #DEBUG#
       if sq != '-': # check if forbidden
         if (ii,jj) in UD:
-          count= count_in_line([state[ii+n][jj] for n in range(K_WIN)])
+          count= count_in_line([board[ii+n][jj] for n in range(K_WIN)])
           if count[SIDE]>0: hs+=10**(count[SIDE]-1) #add player's count
           if count[(SIDE+1)%2]>0: hs-=10**(count[(SIDE+1)%2]-1) #subtract opponent's count
-          print("ud window:\t"+str([state[ii+n][jj] for n in range(K_WIN)])) #DEBUG#
-          print("count:\t"+str(count))
+          # print("ud window:\t"+str([state[ii+n][jj] for n in range(K_WIN)])) #DEBUG#
+          # print("count:\t"+str(count))
         if (ii,jj) in LR:
-          count= count_in_line([state[ii][jj+n] for n in range(K_WIN)])
+          count= count_in_line([board[ii][jj+n] for n in range(K_WIN)])
           if count[SIDE]>0: hs+=10**(count[SIDE]-1) #add player's count
           if count[(SIDE+1)%2]>0: hs-=10**(count[(SIDE+1)%2]-1) #subtract opponent's count
-          print("lr window:\t"+str([state[ii][jj+n] for n in range(K_WIN)])) #DEBUG#
-          print("count:\t"+str(count))
+          # print("lr window:\t"+str([state[ii][jj+n] for n in range(K_WIN)])) #DEBUG#
+          # print("count:\t"+str(count))
         if (ii,jj) in D1:
-          count= count_in_line([state[ii+n][jj-n] for n in range(K_WIN)])
+          count= count_in_line([board[ii+n][jj-n] for n in range(K_WIN)])
           if count[SIDE]>0: hs+=10**(count[SIDE]-1) #add player's count
           if count[(SIDE+1)%2]>0: hs-=10**(count[(SIDE+1)%2]-1) #subtract opponent's count
-          print("d1 window:\t"+str([state[ii+n][jj-n] for n in range(K_WIN)])) #DEBUG#
-          print("count:\t"+str(count))
+          # print("d1 window:\t"+str([state[ii+n][jj-n] for n in range(K_WIN)])) #DEBUG#
+          # print("count:\t"+str(count))
         if (ii,jj) in D2:
-          count= count_in_line([state[ii+n][jj+n] for n in range(K_WIN)])
+          count= count_in_line([board[ii+n][jj+n] for n in range(K_WIN)])
           if count[SIDE]>0: hs+=10**(count[SIDE]-1) #add player's count
           if count[(SIDE+1)%2]>0: hs-=10**(count[(SIDE+1)%2]-1) #subtract opponent's count
-          print("d2 window:\t"+str([state[ii+n][jj+n] for n in range(K_WIN)])) #DEBUG#
-          print("count:\t"+str(count))
+          # print("d2 window:\t"+str([state[ii+n][jj+n] for n in range(K_WIN)])) #DEBUG#
+          # print("count:\t"+str(count))
 
   return hs
 
-def get_successors(state,whoseMove):
-  ''' whose move is either 'x' or 'o' '''
-  successors=[]
+def get_successors(state,who):
+  global DIM
+  ''' who is either 'x' or 'o' '''
+  successors=[];
   for ii in range(DIM[0]):
     for jj in range(DIM[1]):
-      newState=state[:]
-      if state[ii][jj] == ' ':
-        newState[ii][jj] = whoseMove.lower()
+
+      newState=deepcopy(state)
+
+      if newState[ii][jj] == ' ':
+        newState[ii][jj] = who
         successors.append(newState)
+        # print("newstate \t"+str(newState)) #DEBUG#
 
   return successors
 
@@ -146,31 +184,20 @@ def count_in_line(list):
 
 def init_valid_square_lists():
   global K_WIN,DIM,UD,LR,D1,D2
-  if DIM[0] < K_WIN or DIM[1] < K_WIN:
-    raise ValueError("K greater than DIM" )
-    return
 
-# prepare valid win index list
-#   k=K_WIN
-#   UD=[ii*DIM[1]+jj for ii in range(DIM[0]-k) for jj in range(DIM[1])]
-#   LR=[ii*DIM[1]+jj for ii in range(DIM[0]) for jj in range(DIM[1]-k)]
-#   D1=[ii*DIM[1]+jj for ii in range(DIM[0]-k) for jj in range(K-1,DIM[1])]
-#   D2=[ii*DIM[1]+jj for ii in range(DIM[0]-k) for jj in range(DIM[1]-k)]
-
-#DEBUG#
   K=K_WIN
   UD=[(ii,jj) for ii in range(DIM[0]-K+1) for jj in range(DIM[1])]
   LR=[(ii,jj) for ii in range(DIM[0]) for jj in range(DIM[1]-K+1)]
   D1=[(ii,jj) for ii in range(DIM[0]-K+1) for jj in range(K-1,DIM[1])]
   D2=[(ii,jj) for ii in range(DIM[0]-K+1) for jj in range(DIM[1]-K+1)]
-  print("valid start quares for a win per dir")
-  print("up-down: "); print(str(UD))
-  print("LR: "); print(str(LR))
-  print("D1: "); print(str(D1))
-  print("D2: "); print(str(D2))
+  #DEBUG#
+  # print("valid start quares for a win per dir")
+  # print("up-down: "); print(str(UD))
+  # print("LR: "); print(str(LR))
+  # print("D1: "); print(str(D1))
+  # print("D2: "); print(str(D2))
 
   return
-
 
 def init_zobrist():
   global ZOBRIST_VALS,DIM
@@ -186,29 +213,30 @@ def init_default_remarks():
                   OP_NAME+", "+OP_NAME+", "+OP_NAME+"... less talk, more play.",
                   ""]
 
-def z_hash_code(state):
+def z_hash_code(board):
   global ZOBRIST_VALS
 
   val=0; side=None
-  for ii in range(len(state)): #for each square, xor val with rand number from table
-    for jj in range(len(state[0])):
-      if state[ii][jj].lower()=='x': side=1
-      if state[ii][jj].lower()=='o': side=0
+  for ii in range(len(board)): #for each square, xor val with rand number from table
+    for jj in range(len(board[0])):
+      if board[ii][jj].lower()=='x': side=1
+      if board[ii][jj].lower()=='o': side=0
       if side!=None: val ^= ZOBRIST_VALS[ii*DIM[1]+jj][side]
 
 
   return val
 
+def display_board(b):
+  global DIM
 
-# Given a state in the default format, the function returns its 1 x n*m list representation
-def parse_state(state):
-    temp = state[0]                              # board pieces
-    xferState = []
-    for i in range(len(temp)):                   # rows
-        for j in range(len(temp[0])):            # columns
-            xferState.append(temp[i][j])
-    return xferState
-
+  txt="+"+3*DIM[0]*"-"+"+"+"\n"
+  for row in b:
+      txt+="|"
+      for item in row:
+          txt+=" "+item+" "
+      txt+="|\n"
+  txt+="+"+3*DIM[0]*"-"+"+"+"\n"
+  return txt
 
 def introduce():
   return """Hello, my name is (player name). Developed by Bilkit and David,
@@ -216,7 +244,6 @@ def introduce():
 
 def nickname():
   return "player1"
-
 
 #<REMARK>#
 def retort(remark):
@@ -285,16 +312,21 @@ def you_me_map(wordlist):
 
 #<REMARK>#
 
+#<TEST>#
 def test():
-  prepare(game.INITIAL_STATE,game.K,'X',"noname")
-  print("hash init state= "+str(z_hash_code( game.INITIAL_STATE[0] )))
-  print("staticE="+str(staticEval(game.INITIAL_STATE[0])))
-  list=get_successors(game.INITIAL_STATE[0],'x')
-  print("successor: "+str(len(list)))
-  print("other for x: "+get_other('x'))
-  print("other for o: "+get_other('o'))
-  # state = game.INITIAL_STATE[0]
+  state=game.INITIAL_STATE
+  prepare(state,game.K,'X',"noname")
+  # print("hash init state= "+str(z_hash_code( state[0] )))
+  # print("staticE="+str(staticEval(state[0])))
+  # print("successors:\n"+str(len(get_successors(state[0],'x'))))
+  # print("minimax for x: "+str(mini_max(state[0],state[1],2)))
+  state[1]='o'
+  makeMove(state, "")
+  # print("minimax for o: "+str(mini_max(state[0],'o',2)))
+  # print("other for x: "+get_other('x'))
+  # print("other for o: "+get_other('o'))
   # state[0][1]='x'; state[0][2]='o'
   # print("hash state= "+str(z_hash_code( state )))
+#<TEST>#
 
 test()
